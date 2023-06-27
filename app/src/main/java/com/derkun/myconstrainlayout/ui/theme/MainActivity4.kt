343d5c46b061b800fb07d2b1d8dc93924dd75cec
AlexanderDerkun <38619024+AlexanderDerkun@users.noreply.github.com>
2023-06-27 13:47:50 +0300

    add new activity_main4.xml , six buttons with the flow appearance

---
+++ app/src/main/java/com/derkun/myconstrainlayout/ui/theme/MainActivity4.kt
@@ -0,0 +1,12 @@
+package com.derkun.myconstrainlayout.ui.theme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.derkun.myconstrainlayout.R
+
+class MainActivity4 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main4)
+    }
+}
